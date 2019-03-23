fa2826d2137f3719a0c4a16fc9462e1dd57639f8
krzysztof klaczek <klaczek.krzysztof.kriss@gmail.com>
2019-03-23 20:45:49 +0100

    Model mapper

--- src/main/java/com/kklaczek/dentist_web_api/DentistWebApiApplication.java
+++ src/main/java/com/kklaczek/dentist_web_api/DentistWebApiApplication.java
@@ -1,13 +1,20 @@
 package com.kklaczek.dentist_web_api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DentistWebApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DentistWebApiApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
