9abccb8d58b015fd277bb77c8386cec7c241c9b6
krzysztof klaczek <klaczek.krzysztof.kriss@gmail.com>
2019-03-23 15:30:47 +0100

    INIT

---
+++ src/main/java/com/kklaczek/dentist_web_api/DentistWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.kklaczek.dentist_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentistWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentistWebApiApplication.class, args);
+    }
+
+}
