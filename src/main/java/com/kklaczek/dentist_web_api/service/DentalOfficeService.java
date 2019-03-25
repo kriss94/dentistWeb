267536968f35f06edee1ad1e55e273aed0aab85b
krzysztof klaczek <klaczek.krzysztof.kriss@gmail.com>
2019-03-25 22:37:50 +0100

    Implements crud address, category, dentalOffice, information Service and fix small bug in CategoryRepository

--- src/main/java/com/kklaczek/dentist_web_api/service/DentalOfficeService.java
+++ src/main/java/com/kklaczek/dentist_web_api/service/DentalOfficeService.java
@@ -1,4 +1,10 @@
 package com.kklaczek.dentist_web_api.service;
 
+import com.kklaczek.dentist_web_api.dto.DentalOfficeDto;
+
 public interface DentalOfficeService {
+    DentalOfficeDto save(DentalOfficeDto dentalOfficeDto);
+    DentalOfficeDto findById(Long id);
+    DentalOfficeDto edit(Long id, DentalOfficeDto dentalOfficeDto);
+    DentalOfficeDto delete(Long id);
 }
--- src/main/java/com/kklaczek/dentist_web_api/serviceImpl/InformationServiceImpl.java
+++ src/main/java/com/kklaczek/dentist_web_api/serviceImpl/InformationServiceImpl.java
@@ -1,6 +1,42 @@
 package com.kklaczek.dentist_web_api.serviceImpl;
 
+import com.kklaczek.dentist_web_api.dto.InformationDto;
+import com.kklaczek.dentist_web_api.entity.Information;
+import com.kklaczek.dentist_web_api.repository.InformationRepository;
 import com.kklaczek.dentist_web_api.service.InformationService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class InformationServiceImpl implements InformationService {
+
+    private final ModelMapper modelMapper;
+    private final InformationRepository informationRepository;
+
+    @Override
+    public InformationDto save(InformationDto informationDto) {
+        Information information = modelMapper.map(informationDto, Information.class);
+        return modelMapper.map(informationRepository.save(information), InformationDto.class);
+    }
+
+    @Override
+    public InformationDto findById(Long id) {
+        return modelMapper.map(informationRepository.findById(id).orElseThrow(NullPointerException::new), InformationDto.class);
+    }
+
+    @Override
+    public InformationDto edit(Long id, InformationDto informationDto) {
+        Information information = modelMapper.map(informationDto, Information.class);
+        information.setId(id);
+        return modelMapper.map(informationRepository.save(information), InformationDto.class);
+    }
+
+    @Override
+    public InformationDto delete(Long id) {
+        InformationDto informationDto = modelMapper.map(informationRepository.findById(id).orElseThrow(NullPointerException::new),InformationDto.class);
+        informationRepository.deleteById(id);
+        return informationDto;
+    }
 }
